516efda8ee1b2acc06c3deade9492b4b813ea0bc
dominic <dominicassia@gmail.com>
2020-10-15 20:15:42 -0400

    Working on pin assignment

---
+++ 7 Segment Display (4by1).ino
@@ -0,0 +1,36 @@
+/* 
+  Cathodes
+  Digit 1 --> Digit 4
+  D9, D10, D11, D12
+  
+  Anodes
+  D0, D1, D2, D4, D5, D6, D7, D8
+
+  2  --> G
+  3  --> C
+  4  --> DP
+  5  --> D
+  6  --> B
+  7  --> F
+  8  --> A
+  13 --> E
+*/
+
+int digit_1 = 12;
+int digit_2 = 11;
+int digit_3 = 10;
+int digit_4 = 9;
+
+void allLoop() {
+
+}
+
+
+void setup() {
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+}
